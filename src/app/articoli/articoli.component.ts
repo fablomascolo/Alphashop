7393042af78d2b0326a0be193ab28ede827164e3
fabrizio.lomascolo <fablomascolo@yahoo.it>
2021-01-26 19:53:48 +0100

    Commit del 26 gennaio

--- src/app/articoli/articoli.component.ts
+++ src/app/articoli/articoli.component.ts
@@ -27,7 +27,7 @@ export class ArticoliComponent implements OnInit {
     new Articoli('014600301','BARILLA FARINA 1 KG','PZ',24,1,1.09,true,new Date()),
     new Articoli('013500121','BARILLA PASTA GR.500 N.70 1/2 PENNE','PZ',30,0.5,1.3,true,new Date()),
     new Articoli('007686402','FINDUS FIOR DI NASELLO 300 GR','PZ',8,0.3,6.46,true,new Date()),
-    new Articoli('057549001','FINDUS CROCCOLE 400 GR','PZ',12,0.4,5.97,true,new Date())
+    new Articoli('057549001','FINDUS CROCCOLEx 400 GR','PZ',12,0.4,5.97,true,new Date())
   ]
 
   constructor() { }
